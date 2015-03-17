38b5549f397c5c0c2ba5f2a57b7e17444d54e5fc
andrewh-code <andrewh.code@gmail.com>
2015-03-16 23:47:26 -0400

    updating Challenge2.java

--- java_stuff/Matasano_Crypto_Challenge/src/set1/Challenge2.java
+++ java_stuff/Matasano_Crypto_Challenge/src/set1/Challenge2.java
@@ -1,88 +1,55 @@
 package set1;
-import java.util.*;
+import Libraries.CryptoConversions;
 
+/*
+ * Write a function that takes two equal-length buffers and produces their XOR combination.
+
+    If your function works properly, then when you feed it the string:
+    
+    1c0111001f010100061a024b53535009181c
+    ... after hex decoding, and when XOR'd against:
+    
+    686974207468652062756c6c277320657965
+    ... should produce:
+    
+    746865206b696420646f6e277420706c6179
+ 
+ */
 
 public class Challenge2
 {
-    
-    /*
-     * Write a function that takes two equal-length buffers and produces their XOR combination.
-
-        If your function works properly, then when you feed it the string:
-        
-        1c0111001f010100061a024b53535009181c
-        ... after hex decoding, and when XOR'd against:
-        
-        686974207468652062756c6c277320657965
-        ... should produce:
-        
-        746865206b696420646f6e277420706c6179
-     
-     */
-    
     //take two base 64 strings, XOR them against each other
     public String Base64FixedXOR(String input, String mask){
-        
+       
       //make sure that both strings have the same length
         if (input.length() != mask.length()){
             System.out.println("String lengths do not match. Both strings must have the same length.\n");
             System.exit(1);
         }
         
+        //instantiate new objects
+        CryptoConversions crypto   = new CryptoConversions();
+        StringBuilder finalMessage = new StringBuilder();
         //declare variables
-        int i = 0;
-        int size = input.length();
-
-        int[] arrayInput = new int[size];
-        int[] arrayMask = new int[size];
-        int arrayResult;
+        int i 					= 0;
+        int size 				= input.length();
+        byte[] arrayInput 		= new byte[size];
+        byte[] arrayMask 		= new byte[size];
+        byte[] arrayXORResult 	= new byte[size];
+        
         String output = "";
-        arrayInput = HexToDec(input);
-        arrayMask = HexToDec(mask);
-        
-        for (i = 0; i < size/2; i++){
-            arrayResult = (arrayInput[i] ^ arrayMask[i]);
-            output = DecToHex(arrayResult);
-            
-            System.out.println(arrayInput[i] + "\t" + arrayMask[i] + "\t" + arrayResult + "\t" + output + "\n");
-            //output += table[arrayResult[i]];
-        }
-        
-        
-        return output;
-    }
-    
 
-    public int[] HexToDec(String input){
-        
-        int[] array = new int[input.length()/2];
-        int i = 0;
-        int temp;
-        
-        for (i = 0; i < input.length() -2; i= i+2){
-            temp = Integer.parseInt(input.substring(i, i+2), 16);
-            array[i/2] = temp;
+        //convert input to byte array
+        for (i = 0; i < input.length(); i++){
+        	arrayInput[i] = (byte)crypto.HexToDec(input.substring(i, i+1));
+        	arrayMask[i] = (byte)crypto.HexToDec(mask.substring(i, i+1));
+        	arrayXORResult[i] = (byte)(arrayInput[i] ^ arrayMask[i]);
+        	
+        	//convert byte/integer to hex value
+        	finalMessage.append(Integer.toHexString(arrayXORResult[i]));
+        	//System.out.println(arrayInput[i] + "\t" + arrayMask[i] + "\t" + arrayXORResult[i] + "\t" + finalMessage + "\n");
         }
-        
-        return array;
-    }
-    
-    public String DecToHex(int input){
-        
-        String HEX_TABLE = "0123456789ABCDEF";
-        int dividend = input;
-        int remainder = 1;
-        int result = 0;
-        String output = "";
-        
-        //TO DO: figure out how to do this
-        while (remainder >= 16){
-            remainder = dividend % 16;
-            result = (int)(dividend / 16);
-            dividend = result * 16;
-            output = HEX_TABLE.charAt(remainder) + output;
-            
-        }
+        output = finalMessage.toString();
         
         return output;
     }
